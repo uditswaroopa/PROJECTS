82595126afd687eb40a074e442ef411acf6ee416
Udit <uditswaroopa@icloud.com>
2020-12-06 22:09:19 +0530

    Simple Bloc App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:PROJECTS/counter_bloc.dart';
-import 'package:PROJECTS/counter_bloc_event.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,6 +21,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -29,7 +29,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _bloc = Counter();
+  int _counter = 0;
+
+  final bloc = CounterBloc();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,39 +40,31 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: StreamBuilder(
-            stream: _bloc.outCounter,
-            initialData: 0,
-            builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    '${snapshot.data}',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                ],
-              );
-            }),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder(
+              stream: bloc.counterStream,
+              builder: (context, snapshot) {
+                return Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () => _bloc.counterEvent.add(IncrementEvent()),
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(width: 15.0),
-          FloatingActionButton(
-            onPressed: () => _bloc.counterEvent.add(DecrementEvent()),
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _counter++;
+          bloc.counterSink.add(_counter);
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
